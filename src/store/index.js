849e3caebc1a4fc3a09a52d16682c5a52562ce34
ardaalkan <ardaaa.alk@gmail.com>
2022-12-28 19:06:32 +0300

    added secure-ls package to state

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,17 @@
 import { createStore } from "vuex";
 import createPersistedState from "vuex-persistedstate";
+import SecureLS from "secure-ls";
+var ls = new SecureLS({ isCompression: false });
 
 export default createStore({
   state: {
     user: null,
     saltKey: "booklike123!456",
   },
-  mutations : {
-    setUser(state, user){
-      state.user = user
-    }
+  mutations: {
+    setUser(state, user) {
+      state.user = user;
+    },
   },
   getters: {
     _isAuthenticated: (state) => state.user !== null,
@@ -23,5 +25,13 @@ export default createStore({
     },
     _saltKey: (state) => state.saltKey,
   },
-  plugins : [createPersistedState()]
+  plugins: [
+    createPersistedState({
+      storage: {
+        getItem: (key) => ls.get(key),
+        setItem: (key, value) => ls.set(key, value),
+        removeItem: (key) => ls.remove(key),
+      },
+    }),
+  ],
 });
