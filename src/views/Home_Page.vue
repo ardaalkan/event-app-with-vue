401de3607fd0abf21f9d65dc3038af654f7795f4
ardaalkan <ardaaa.alk@gmail.com>
2023-01-06 02:32:49 +0300

    added new assets to design process

--- src/views/Home_Page.vue
+++ src/views/Home_Page.vue
@@ -1,40 +1,7 @@
 <template>
   <NavbarView />
-  <div class="masonary_view">
+  <div>
     <h1 class="home_text">Home Page <span>E-Commerce</span></h1>
-    <span class="blob-background-component">
-      <!-- <svg width="1000" height="1000" viewBox="0 0 1000 1000">
-        <defs>
-          <filter id="grain" x="-50vw" y="-50vh" width="100vw" height="100vh">
-            <feFlood flood-color="#ffffff" result="neutral-gray" />
-
-            <feTurbulence in="neutral-gray" type="fractalNoise" baseFrequency="2.5" numOctaves="100" stitchTiles="stitch" result="noise" />
-
-            <feColorMatrix in="noise" type="saturate" values="0" result="destaturatedNoise"></feColorMatrix>
-
-            <feComponentTransfer in="desaturatedNoise" result="theNoise">
-              <feFuncA type="table" tableValues="0 0 0.1 0"></feFuncA>
-            </feComponentTransfer>
-
-            <feBlend in="SourceGraphic" in2="theNoise" mode="soft-light" result="noisy-image" />
-          </filter>
-
-          <clipPath id="shape">
-            <path
-              fill="currentColor"
-              d="M855,614.5Q773,729,677,845Q581,961,482,832Q383,703,259,668Q135,633,137,501Q139,369,240,297Q341,225,457.5,154Q574,83,651.5,195.5Q729,308,833,404Q937,500,855,614.5Z"
-            ></path>
-          </clipPath>
-        </defs>
-
-        <g filter="url(#grain)" clip-path="url(#shape)">
-          <path
-            fill="#e9fff6"
-            d="M855,614.5Q773,729,677,845Q581,961,482,832Q383,703,259,668Q135,633,137,501Q139,369,240,297Q341,225,457.5,154Q574,83,651.5,195.5Q729,308,833,404Q937,500,855,614.5Z"
-          />
-        </g>
-      </svg> -->
-    </span>
   </div>
   <FooterView />
 </template>
@@ -59,13 +26,6 @@
 .home_img {
   height: 15rem;
   margin-left: 10px;
-}
-
-.masonary_view {
-  display: flex;
-  flex-direction: column;
-  /* align-items: center; */
-  margin: auto;
 }
 
 .blop-background-component {
